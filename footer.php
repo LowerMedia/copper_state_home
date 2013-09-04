86d3a8819568337a25048b78c87526073c4cf734
Pete Lower <pete.lower@gmail.com>
2013-09-04 16:22:50 -0500

    adding style and footer files

---
+++ footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package Expound
+ */
+?>
+
+	</div><!-- #main -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<center>
+			Phoenix Home Inspections | Copper State Home Inspections<br/>
+			16619 N 4th Ave<br/>
+			Phoenix, AZ 85027<br/>
+			602-296-4748<br/>
+		</center>
+		</div><!-- .site-info -->
+		<div class="dev-info">
+			<a href="lowermedia.net">A LowerMedia Site</a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
